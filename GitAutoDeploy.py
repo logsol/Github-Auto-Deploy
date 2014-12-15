logsol/Github-Auto-Deploy

commit e7ea26c5a629431ee35763e1e70085aa8cb8b450

Support remote work dir
When a Git repository is created with a remote work directory, there is
no .git folder in the repository folder. Check for the objects directory
as well.

GitAutoDeploy.py
@@ -27,7 +27,9 @@ class GitAutoDeploy(BaseHTTPRequestHandler):
             for repository in myClass.config['repositories']:
                 if(not os.path.isdir(repository['path'])):
                     sys.exit('Directory ' + repository['path'] + ' not found')
-                if(not os.path.isdir(repository['path'] + '/.git')):
+                # Check for a repository with a local or a remote GIT_WORK_DIR
+                if not os.path.isdir(os.path.join(repository['path'], '/.git')) \
+                   and not os.path.isdir(os.path.join(repository['path'], 'objects')):
                     sys.exit('Directory ' + repository['path'] + ' is not a Git repository')
 
         return myClass.config

commit 21e31963cab38c720eb5ad81127ef3d8aadbf818

Fix bug #24 introduced in e7ea26c (Support remote work dir)

GitAutoDeploy.py
@@ -28,7 +28,7 @@ class GitAutoDeploy(BaseHTTPRequestHandler):
                 if(not os.path.isdir(repository['path'])):
                     sys.exit('Directory ' + repository['path'] + ' not found')
                 # Check for a repository with a local or a remote GIT_WORK_DIR
-                if not os.path.isdir(os.path.join(repository['path'], '/.git')) \
+                if not os.path.isdir(os.path.join(repository['path'], '.git')) \
                    and not os.path.isdir(os.path.join(repository['path'], 'objects')):
                     sys.exit('Directory ' + repository['path'] + ' is not a Git repository')
 